86d95b551a49815fa03c1e63d5c2aa912572ec8b
HP <1833176923@qq.com>
2019-09-07 13:40:02 +0800

    first commit

---
+++ config-client/src/main/java/com/pricemonitor/configclient/HelloController.java
@@ -0,0 +1,19 @@
+package com.pricemonitor.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class HelloController {
+    @Value("${test.config.bus}")
+    private String hello;
+
+    @GetMapping("/hello")
+    public String from() {
+        System.out.println("获取配置文件信息");
+        return this.hello;
+    }
+}
